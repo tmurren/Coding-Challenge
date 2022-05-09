907cb6c136c0a337ef410444ef27c410ff4f9f9a
tmurren <t_murren@msn.com>
2022-05-09 12:27:48 -0400

    Update Spacing
    Strange tab spacing found on file

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@
 // #################### THREE UP BUTTON TOGGLES ####################
 // on click, this toggles the fontawesome icon class name, and the content of the span from view to hide
 $('.three-up__item__button').click(function() {
-	$(this).find('i').toggleClass('fa-eye fa-eye-slash');
+   $(this).find('i').toggleClass('fa-eye fa-eye-slash');
    $(this).find('span').text($(this).find('span').text() == 'View' ? 'Hide' : 'View');
 });
 
